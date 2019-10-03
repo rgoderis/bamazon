f3ff643f905ceaf6585e4fbac9a9794d4d45e448
Ryan Goderis <rgoderis789@gmail.com>
2019-10-03 19:17:44 -0400

    made startBamazon function to start or exit application, fixed syntax error in UPDATE section of buyProducts function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,13 +22,75 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if(err) throw err
     console.log("connected")
-    loadProducts()
-    connection.end()
+    startBamazon()
 });
 
+function startBamazon(){
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "please select [Products] to view and purcase products or [Exit] to exit application",
+            choices: ["Products", "Exit"],
+            name: "choice"
+        }
+    ]).then(function(answer){
+        if(answer.choice === "Products"){
+            loadProducts();
+        } else{
+            connection.end();
+        }
+    })
+}
+
 function loadProducts(){
+    // display all products available to purchase
     connection.query("SELECT * FROM products", function(err, res){
         if(err) throw err
         console.table(res)
+        // run function to allow customer to purchase a product
+        buyProducts()
     })
+}
+
+function buyProducts(){
+    // prompt user with id of product they would like to purchase and amount
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the id of the product you would like to purchase?",
+            name: "productId"
+        },
+        {
+            type: "input",
+            messge: "How many would you like to purchase?",
+            name: "purchaseAmount"
+        }
+    ]).then(function(answers){
+        // retrieve stock_quantity by id
+        connection.query("SELECT * FROM products WHERE ?", {id: answers.productId}, function(err, res){
+            if(err) throw err
+            // if stock_quantity is >= answers.purchaseAmount
+            if(res[0].stock_quantity >= parseInt(answers.purchaseAmount)){
+                //  subtract stock_quantity by answers.purchaseAmount
+                var newQuantity = res[0].stock_quantity - parseInt(answers.purchaseAmount)
+                connection.query("UPDATE products SET ? WHERE ?",
+                [
+                        {
+                            stock_quantity: newQuantity
+                        },
+                        {
+                            id: answers.productId
+                        }
+                ], function(err){
+                    if(err) throw err
+                    console.log("Item purchased")
+                    startBamazon()
+                })
+            } else{
+                // else log error message and run buyProducts()
+                console.log("You can't buy that many")
+                buyProducts()
+            }        
+        })
+    })  
 }
