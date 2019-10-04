fe3fb0860fced82eb44360b6032a6bca866c2042
Ryan Goderis <rgoderis789@gmail.com>
2019-10-04 15:43:33 -0400

    created bamazonManager.js, required mysql and inquirer, created connection to bamazondb

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,8 @@
-// display items
-// prompt user for ID
-// please enter quantity
-// check order
-    // if not enough log err
-    // else update database
-        // show total cost
-// done
-// restart app
 
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     post: "localhost",
     port: 3306,
     user: "root",
---
+++ bamazonManager.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    post: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "bamazon"
+});
+
+connection.connect(function(err){
+    if(err) throw err
+    console.log("connected");
+    bamazonManager()
+});
