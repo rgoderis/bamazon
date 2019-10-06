9e902884dbd5b62d8b10c7a53c2d7af72d7a187f
Ryan Goderis <rgoderis789@gmail.com>
2019-10-06 13:34:04 -0400

    created bamazonSupervisor.js, required mysql and inquirer, set connection to bamazon database, created function bamazonSupervisor

---
+++ bamazonSupervisor.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    post: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "bamazon"
+});
+
+connection.connect(function(err){
+    if(err) throw err
+    console.log("connected")
+    bamazonSupervisor()
+});
+
+function bamazonSupervisor(){
+    
+}
