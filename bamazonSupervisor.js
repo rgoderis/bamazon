4dee140e6e55d313af77c048c531da2b4236d019
Ryan Goderis <rgoderis789@gmail.com>
2019-10-06 13:40:45 -0400

    set inquirer prompt and switch in bamazonSupervisor for each choice

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -16,5 +16,21 @@ connection.connect(function(err){
 });
 
 function bamazonSupervisor(){
-    
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View Product Sales by Department", "Create New Department", "Exit"],
+            name: "choice"
+        }
+    ]).then(function(answer){
+        switch(answer.choice){
+            case "View Product Sales by Department":
+                return productSales();
+            case "Create New Department":
+                return newDepartment();
+            case "Exit":
+                return connection.end();
+        }
+    })
 }
